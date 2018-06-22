14a361c5891c7d930d1d6adf7d242eac34a2dd9d
yurikrupnik <krupnik.yuri@gmail.com>
2018-06-22 20:31:51 +0300

    init

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -86,11 +86,11 @@ module.exports = {
             chunkFilename: '[name].css'
         }),
         // new CompressionPlugin(),
-        new BundleAnalyzerPlugin({
-            //     // openAnalyzer: false,
-            //     // analyzerMode: 'static',
-            //     // generateStatsFile: true,
-            //     // statsFilename: 'clientBundleAnalyzer.json'
-        })
+        // new BundleAnalyzerPlugin({
+        //     // openAnalyzer: false,
+        //     // analyzerMode: 'static',
+        //     // generateStatsFile: true,
+        //     // statsFilename: 'clientBundleAnalyzer.json'
+        // })
     ]
 };
